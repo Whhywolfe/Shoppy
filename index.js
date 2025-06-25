706b9526c0344312d0e6329dad1c0206506d291f
Whhywolfe <whhywolfee@gmail.com>
2025-06-25 20:40:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const http = require('http');
 
+// Firebase/Cloud Run sets this env variable automatically
+const PORT = process.env.PORT || 8080;
+
 const server = http.createServer((req, res) => {
   res.end('Hello from SkyHigh Sim backend!');
 });
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
